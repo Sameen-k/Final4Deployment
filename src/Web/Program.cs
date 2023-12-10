35dae85a5457449aea61676c38e5a0dc638d6817
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-12-10 18:35:40 -0500

    Update Program.cs

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -28,19 +28,7 @@ if (builder.Environment.IsDevelopment() || builder.Environment.EnvironmentName =
 }
 else{
     Microsoft.eShopWeb.Infrastructure.Dependencies.ConfigureServices(builder.Configuration, builder.Services
-    // Configure SQL Server (prod)
-    //var credential = new ChainedTokenCredential(new AzureDeveloperCliCredential(), new DefaultAzureCredential());
-   // builder.Configuration.AddAzureKeyVault(new Uri(builder.Configuration["AZURE_KEY_VAULT_ENDPOINT"] ?? ""), credential);
-    //builder.Services.AddDbContext<CatalogContext>(c =>
-    //{
-        //var connectionString = builder.Configuration[builder.Configuration["AZURE_SQL_CATALOG_CONNECTION_STRING_KEY"] ?? ""];
-        //c.UseSqlServer(connectionString, sqlOptions => sqlOptions.EnableRetryOnFailure());
-    //});
-    //builder.Services.AddDbContext<AppIdentityDbContext>(options =>
-    //{
-        //var connectionString = builder.Configuration[builder.Configuration["AZURE_SQL_IDENTITY_CONNECTION_STRING_KEY"] ?? ""];
-        //options.UseSqlServer(connectionString, sqlOptions => sqlOptions.EnableRetryOnFailure());
-    //});
+
 }
 
 builder.Services.AddCookieSettings();
