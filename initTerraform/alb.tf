cd6b5e655bdb2dca8cdabcb7f57a0c975a54cc1a
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-12-11 19:29:59 -0500

    Update alb.tf

--- initTerraform/alb.tf
+++ initTerraform/alb.tf
@@ -25,7 +25,7 @@ resource "aws_alb" "ALB" {
     aws_subnet.publicC.id
   ]
 
-  security_groups = [sg-01f1382b32a3a784d]
+  security_groups = [aws_security_group.alb_sg.id]
 
   depends_on = [aws_internet_gateway.final4_igw]
 }
@@ -46,3 +46,54 @@ resource "aws_alb_listener" "listener" {
 output "alb_url" {
   value = "http://${aws_alb.ALB.dns_name}"
 }
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = aws_vpc.final4_vpc.id  # Replace with your VPC ID
+
+  # Allow HTTP (port 80)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow HTTPS (port 443)
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow custom port 3000
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow custom port 8000
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Default egress rule: Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
